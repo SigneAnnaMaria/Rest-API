a0a47a67d58e780ea995243bc9eb1dfd6829a86d
SigneAnnaMaria <signe.annamaria@gmail.com>
2023-01-09 13:14:19 +0100

    new api - finished

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Rest-API
+
+Titel: 
+REST-API - Anställda
+
+Krav uppfyllda: 
+G
+
+Beskrivning av projektet och uppbyggnaden samt körs:
+
+Server.js är källan med olika endpoints som uppdaterar db.json när förfrågning/ändringar görs via send request i users.rest som visar om förfrågningen/ändringen går bra eller dåligt med 404. Om förfrågan/ändringen går bra får vi en text new employee added/updated employee/deleted employee och du behöver gå till db.json för att se förändringen.
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,3 +1,80 @@
-const express = require("express")
+const express = require('express')
 const app = express()
+const port = 3000
+const fs = require('fs')
 
+var bodyParser = require('body-parser')
+let db = 'db.json'
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Send list of employees
+app.get('/employees', (req, res) => {
+  fs.readFile(db, 'utf-8', (err, data) => {
+    if (err) {
+      res.status(404).send(err)
+    }
+    res.send(JSON.parse(data))
+  })
+})
+
+//Add employee Frans Fritz to list
+app.post('/employees', (req, res) => {
+  fs.readFile(db, 'utf8', (err, data) => {
+      const newUserId = Date.now().toString()
+      let obj = JSON.parse(data)
+      obj[newUserId] = req.body
+      let json = JSON.stringify(obj, null, 2)
+
+      fs.writeFile(db, json, (err) => {
+        if (err) {
+          res.status(404).send(err)
+        } else {
+          res.status(200).send('new employee added')
+        }
+      })
+    },
+    true
+  )
+})
+
+//Update employee first in list to Emma Enoksson 
+app.put('/employees/:id', (req, res) => {
+  fs.readFile(db, 'utf8', (err, data) => {
+    const uniqueID = parseInt(req.params.id)
+    let obj = JSON.parse(data)
+    obj[uniqueID] = req.body
+    let json = JSON.stringify(obj, null, 2)
+
+    fs.writeFile(db, json, (err) => {
+      if (err) {
+        res.status(404).send(err)
+      } else {
+        res.status(200).send('updated employee')
+      }
+    })
+  })
+})
+
+//Delate employee first in list
+app.delete('/employees/delete/:id', (req, res) => {
+  fs.readFile(db, 'utf8', (err, data) => {
+    const uniqueID = parseInt(req.params.id)
+    let obj = JSON.parse(data)
+    delete obj[uniqueID]
+    let json = JSON.stringify(obj, null, 2)
+    
+    fs.writeFile(db, json, (err) => {
+      if (err) {
+        res.status(404).send(err)
+      } else {
+        res.status(200).send('deleted employee')
+      }
+    })
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
